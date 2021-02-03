731e89cb80923a0833ea67e246b4c20e4ceda3c7
Vennah <78482517+Vennah@users.noreply.github.com>
2021-02-03 20:42:20 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset"UTF-8">
+    <meta name="viewpoint" content="width=device-width, inital-scale=1.0>
+    <title>Document</title>
+</head>
+<body>
+    <p>Welcome to my page [Vennah Lavinia Magbanua] </p>
+</body>
+</html>
